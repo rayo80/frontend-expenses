992f869be42a34517c0e9970c4643e36e0da2d4c
Sergio <sergiovilla242@gmail.com>
2024-05-28 15:00:04 -0500

    add filereplacements

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from 'app/app.component';
 import { appConfig } from 'app/app.config';
+import { environment } from 'environments/environment';
 
+console.log("no entro")
+
+if (environment.production) {
+    console.log("aquisi")
+    enableProdMode();
+  }
+  
 bootstrapApplication(AppComponent, appConfig)
     .catch(err => console.error(err));
